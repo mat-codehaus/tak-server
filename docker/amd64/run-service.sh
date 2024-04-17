c3265ccbe29c612582c0c063f35438ec968b00f5
Mathew Pink <mat@codehaus.co>
2024-04-17 14:35:11 +0930

    zt changes

--- docker/amd64/run-service.sh
+++ docker/amd64/run-service.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
+handle_sigterm() {
+    echo "SIGTERM received, performing cleanup..."
+    # Kill the process group, sending SIGTERM to the entire pipeline
+    kill -- -$$
+}
+
+trap handle_sigterm INT TERM
+
+# Start the pipeline in a background process and use `exec` to replace the shell with it
+# This makes it run in the current process group, allowing us to kill it later
 /opt/tak/configureInDocker.sh init >> /opt/tak/logs/takserver.log
+wait $!
+
+
